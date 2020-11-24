dc748627dd751220331d74aa49db7419b702a81b
Idiake God'spower <idiakeg@gmail.com>
2020-11-24 17:34:49 +0100

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,15 +12,6 @@ import { StaticRouter } from 'react-router-dom'
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'water bill', amount: 4500}))
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}))
-store.dispatch(addExpense({description: 'rent', amount: 10950}))
-
-
-// const state = store.getState()
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-// console.log(visibleExpenses)
-
 const jsx = (
   <div>
     <Provider store={store}>
